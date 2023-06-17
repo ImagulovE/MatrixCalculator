4e109ab9394a38db1c7c1c07aa70b1c9d0afdb64
Eldar <imagulov0e@gmail.com>
2023-06-17 11:14:49 +0300

    Добавил проверки для матриц и выполняемого действия над ними

--- MatrixCalculator/Header.h
+++ MatrixCalculator/Header.h
@@ -4,7 +4,8 @@ using namespace std;
 
 enum class ErrorCode {
     NoError, MatrixSizeOutOfRange, MissingNumber, UnknownOperation, DifferentSizesOfMatrices,
-    MatrixInconsistency, InabilityToMulMatrixByNumber, MatrixIsNotSquare
+    MatrixInconsistency, InabilityToMulMatrixByNumber, MatrixIsNotSquare, NumberOfMatricesNotCorrespondOperation,
+    IncorrectInputData
 };
 
 // Функция для чтения матрицы из файла
@@ -13,7 +14,17 @@ vector<vector<int>> readMatrixFromFile(ifstream& file);
 vector<int> splitStringToInt(const string& str);
 // Функция для проверки матриц на корректность
 ErrorCode isMatrixCorrect(const std::vector<std::vector<int>>& matrix1, const std::vector<std::vector<int>>& matrix2, std::string operation);
-// Функция для проверки строк матрицы на присутствие всех чисел
-bool hasMissingNumbers(const std::vector<std::vector<int>>& matrix);
 // Функция для проверки размера матрицы
 bool isMatrixSizeInvalid(const std::vector<std::vector<int>>& matrix);
+// Функция для проверки строк матрицы на присутствие всех чисел
+bool hasMissingNumbers(const std::vector<std::vector<int>>& matrix);
+// Функция для проверки вхождения операции в список допустимых
+bool isOperationInvalid(const std::string& operation);
+// Функция для проверки одинаковости размерностей матриц
+bool areMatricesSameSize(const std::vector<std::vector<int>>& matrix1, const std::vector<std::vector<int>>& matrix2);
+// Функция для проверки возможности умножения двух матриц
+bool areMatricesMultiplicable(const std::vector<std::vector<int>>& matrix1, const std::vector<std::vector<int>>& matrix2);
+// Функция для проверки возможности умножения матрицы на число
+bool abilityToMulMatrixByNumber(const std::vector<std::vector<int>>& matrix1, const std::vector<std::vector<int>>& matrix2);
+// Функция для проверки матрицы на то, является ли она квадратом
+bool isMatrixSquare(const std::vector<std::vector<int>>& matrix);
--- MatrixCalculator/MatrixCalculator.cpp
+++ MatrixCalculator/MatrixCalculator.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 int main() {
-    ifstream file("C:\\Users\\Эльдар\\Desktop\\КиНПО\\MatrixCalculator\\input.txt");
+    ifstream file("input.txt");
     if (!file) {
         cout << "Неверно указан файл с входными данными. Возможно, файл не существует." << endl;
         return 1;
@@ -15,22 +15,24 @@ int main() {
 
     vector<vector<int>> matrix1, matrix2;
     string operation;
+    int counter = 0;
 
     matrix1 = readMatrixFromFile(file);
     matrix2 = readMatrixFromFile(file);
     getline(file, operation);
 
-    if (matrix2[0].empty()) {
+    if (matrix2.empty()) {
         file.clear();
         file.seekg(0, std::ios::beg);
         string line;
         getline(file, line);
-        while (!line.empty()) {
+        while (counter < 200 && !line.empty()) {
             getline(file, line);
+            counter++;
         }
         getline(file, operation);
     }
-
+ 
     // Вывод матриц и действия для проверки правильности считывания данных
     cout << "Matrix 1:" << endl;
     for (const auto& row : matrix1) {
@@ -86,6 +88,11 @@ vector<int> splitStringToInt(const string& str) {
 
 // Функция для проверки матриц на корректность
 ErrorCode isMatrixCorrect(const std::vector<std::vector<int>>& matrix1, const std::vector<std::vector<int>>& matrix2, std::string operation) {
+    // Проверка на то, введена ли хотя бы одна матрица и операция
+    if (matrix1.empty() || operation.empty()) {
+        return ErrorCode::IncorrectInputData;
+    }
+
      // Проверка на размерность матриц
     if (isMatrixSizeInvalid(matrix1) || isMatrixSizeInvalid(matrix2)) {
         return ErrorCode::MatrixSizeOutOfRange;
@@ -96,8 +103,52 @@ ErrorCode isMatrixCorrect(const std::vector<std::vector<int>>& matrix1, const st
         return ErrorCode::MissingNumber;
     }
 
+    // Проверка на то, существует ли введенная операция
+    if (isOperationInvalid(operation)) {
+        return ErrorCode::UnknownOperation;
+    }
+
+    // Проверка на идентичность размеров матриц
+    if (operation == "+" || operation == "-") {
+        if (!areMatricesSameSize(matrix1, matrix2)) {
+            return ErrorCode::DifferentSizesOfMatrices;
+        }
+    }
+
+    // Проверка на согласованность матриц
+    if (operation == "*") {
+        if (!areMatricesMultiplicable(matrix1, matrix2)) {
+            return ErrorCode::MatrixInconsistency;
+        }
+    }
+
+    // Проверка на возможность умножения матрицы на число
+    if (operation == "&") {
+        if (!abilityToMulMatrixByNumber(matrix1, matrix2)) {
+            return ErrorCode::InabilityToMulMatrixByNumber;
+        }
+    }
+
+    // Проверка на квадратность матрицы
+    if (operation == "det" || operation == "^-1") {
+        if (!matrix2[0].empty()) {
+            return ErrorCode::NumberOfMatricesNotCorrespondOperation;
+        }
+        if (!isMatrixSquare(matrix1)) {
+            return ErrorCode::MatrixIsNotSquare;
+        }
+    }
+
     return ErrorCode::NoError;
 }
+
+// Функция для проверки размера матрицы
+bool isMatrixSizeInvalid(const std::vector<std::vector<int>>& matrix) {
+    int numRows = matrix.size();
+    int numCols = matrix[0].size();
+    return numRows > 50 || numCols > 50 || numRows == 0;
+}
+
 
 // Функция для проверки строк матрицы на присутствие всех чисел
 bool hasMissingNumbers(const std::vector<std::vector<int>>& matrix) {
@@ -110,9 +161,43 @@ bool hasMissingNumbers(const std::vector<std::vector<int>>& matrix) {
     return false;
 }
 
-// Функция для проверки размера матрицы
-bool isMatrixSizeInvalid(const std::vector<std::vector<int>>& matrix) {
+// Функция для проверки вхождения операции в список допустимых
+bool isOperationInvalid(const std::string& operation) {
+    std::vector<std::string> validOperations = { "+", "-", "&", "*", "T", "det", "^-1" };
+    for (const auto& validOp : validOperations) {
+        if (operation == validOp) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Функция для проверки идентичности размерностей матриц
+bool areMatricesSameSize(const std::vector<std::vector<int>>& matrix1, const std::vector<std::vector<int>>& matrix2) {
+    int numRows1 = matrix1.size();
+    int numCols1 = matrix1[0].size();
+    int numRows2 = matrix2.size();
+    int numCols2 = matrix2[0].size();
+
+    return numRows1 == numRows2 && numCols1 == numCols2;
+}
+
+// Функция для проверки возможности умножения двух матриц
+bool areMatricesMultiplicable(const std::vector<std::vector<int>>& matrix1, const std::vector<std::vector<int>>& matrix2) {
+    int numCols1 = matrix1[0].size();
+    int numRows2 = matrix2.size();
+
+    return numCols1 == numRows2;
+}
+
+// Функция для проверки возможности умножения матрицы на число
+bool abilityToMulMatrixByNumber(const std::vector<std::vector<int>>& matrix1, const std::vector<std::vector<int>>& matrix2) {
+    return (matrix1[0].size() > 1 || matrix1.size() > 1) && matrix2[0].size() == 1;
+}
+
+// Функция для проверки матрицы на то, является ли она квадратом
+bool isMatrixSquare(const std::vector<std::vector<int>>& matrix) {
     int numRows = matrix.size();
     int numCols = matrix[0].size();
-    return numRows > 50 || numCols > 50 || numRows == 0;
+    return numRows == numCols;
 }
